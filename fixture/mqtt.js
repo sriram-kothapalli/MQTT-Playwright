bbb34cfb6065ea268de602b53a261c984b1e497a
SriramKothapalli <skothapall@phizzle.com>
2024-02-29 15:27:57 +0530

    Last

--- fixture/mqtt.js
+++ fixture/mqtt.js
@@ -1,12 +1,11 @@
 export const expectedData1 = {
     "deviceinfo": {
-        "device_id": 5,
-        "data_available": true,
-        "connection_status": "connected",
-        "test_execution_id": "***",
-        "action": "***",
-        "continuous_run_id": null,
-        "request_id": "***",
-        "is_sampling": false
+        "device_id": "",
+        "serial_number": "****",
+        "model": "****",
+        "currenttime":"****",
+        "location":"****",
+        "status":"***",
+        "comments":"***"
     },
    };
